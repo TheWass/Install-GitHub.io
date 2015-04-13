4e661de092d1e24a8ee6fe8ca7c739915d9d9567
Wass <lord.d35ruct0@gmail.com>
2015-04-13 16:41:28 -0400

    Added custom install path.

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ if ruby -v | egrep -q '^ruby (1\.9|2)'; then
     echo "Ruby installed! Continuing..."
 else
     echo "Github pages requires Ruby 1.9 or higher."
-    read -n 1 -p "Install Ruby using Ruby Version Manager (Y/n)?"
-    [ $REPLY == 'n' ] && { echo 'Please manually install Ruby.' ; exit 1; }
+    read -p "Install Ruby using Ruby Version Manager (Y/n)?"
+    [ "$REPLY" == "n" ] && { echo 'Please manually install Ruby.' ; exit 1; }
     gpg2 -? &> /dev/null && { echo 'Please install gpg (required for RVM)' ; exit 1; }
     command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
     \curl -sSL https://get.rvm.io | bash -s stable --ruby
@@ -15,17 +15,20 @@ fi
 gem install bundler
 bundle install
 echo "Github-pages installed!"
-read -n 1 -p "Initialize website (Y/n)?"
-[ $REPLY == 'n' ] && exit 0
-echo ""
+read -p "Initialize website (Y/n)?"
+[ "$REPLY" == "n" ] && exit 0
+read -p "Enter install folder: " -i "$HOME" -e path
+[ "$path" == "" ] && path="."
 read -p "Enter your GitHub username: " username
+echo "Website will be initialized to $path/$username.GitHub.io"
 # Check if Repo exists
 if curl https://api.github.com/repos/$username/$username.GitHub.io | grep -q '"name": "'$username'.GitHub.io"'; then
     #repo found
-    git clone https://${username}@github.com/$username/$username.GitHub.io.git $username.GitHub.io
+    git clone https://${username}@github.com/$username/$username.GitHub.io.git $path/$username.GitHub.io
 else
+    cd $path
     jekyll new $username.GitHub.io
-    cd $username.GitHub.io
+    $username.GitHub.io
     git init
     git add .
     git commit -m 'Initial commit'
